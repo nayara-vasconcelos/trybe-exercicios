c8b1b63e12247584958042435a79210578193c1e
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2021-08-30 20:02:59 -0300

    Ajustes nos comentários.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte-1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte-1.js
@@ -1,4 +1,4 @@
-//Exercícios - Parte I - Objetos e For/in
+//Exercícios - Parte I - Objetos e For/in.
 
 let info = {
     personagem: 'Margarida',
@@ -6,13 +6,28 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
-// 1) Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo o seu nome.
+/* 
+1) Imprimir no console uma mensagem de boas-vindas para a personagem acima, incluindo o seu nome.
+
+Resultado:
+Bem-vinda, Margarida!
+*/
 
 console.log('Bem-vinda, ' + info.personagem + '!'); // dot notatiom
 
 console.log('Bem-vinda, ' + info['personagem'] + '!'); //Bracket notation
 
-// 2) Insira uma nova propriedade com o nome chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
+/*
+2) Inserir uma nova propriedade com o nome chave 'recorrente' e o valor 'Sim' e, em seguida, imprimir o objeto no console.
+
+Resultado:
+{
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
+}
+*/
 
 info.recorrente = 'Sim';
 console.log(info); // dot notation
@@ -20,7 +35,15 @@ console.log(info); // dot notation
 info['recorrente'] = 'Sim';
 console.log(info); // bracket notation
 
-// 3) Faça um for/in que mostre todas as chaves do objeto.
+/*
+3) Mostrar todas as chaves do objeto.
+
+Resultado: 
+personagem
+origem
+nota
+recorrente 
+*/
 
 for (let key in info){
     console.log(key);
