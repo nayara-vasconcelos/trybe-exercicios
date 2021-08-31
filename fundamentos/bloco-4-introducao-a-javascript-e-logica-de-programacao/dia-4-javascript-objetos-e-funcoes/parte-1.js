43c88c5a539a9cfd7d08cedb29c5408ced83032f
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2021-08-30 21:00:03 -0300

    Exercício 5 da Parte I OK.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte-1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte-1.js
@@ -63,3 +63,27 @@ for (let key in info){
     console.log(info[key]);
 }
 
+/* 
+5) Definir um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Imprimir os valores de cada objeto juntos de acordo com cada uma das chaves.
+
+Resultado:
+Margarida e Tio Patinhas
+Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+Ambos recorrentes
+*/
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178', // Caractere de escape: \'
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+  }
+  
+  for (let key in info) {
+      if (key === 'recorrente' && info[key] === 'Sim' && info2[key] === 'Sim') {
+          console.log('Ambos recorrentes');
+      } else {
+          console.log(info[key] + ' e ' + info2[key]);
+      }
+  }
