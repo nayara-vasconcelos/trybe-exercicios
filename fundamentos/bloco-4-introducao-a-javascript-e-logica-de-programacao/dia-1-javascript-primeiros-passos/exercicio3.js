32dfbe10e4c6f331971e526827c1624471b2cc64
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2021-08-31 21:23:52 -0300

    Exercício 3 OK.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio3.js
@@ -0,0 +1,11 @@
+const a = 9;
+const b = 6;
+const c = 12;
+
+if (a > b && a > c) {
+    console.log(`O maior número é: ${a}`);
+} else if (c > a && c > b) {
+    console.log(`O maior número é: ${c}`);
+} else {
+    console.log(`O maior número é: ${b}`);
+};
