a2653cf52e898449ec57d5179890b94d9d7dd935
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2021-08-31 21:05:02 -0300

    Exercício 1 OK.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio1.js
@@ -0,0 +1,9 @@
+//Programa 1
+const a = 5;
+const b = 4;
+
+console.log(`a + b = ${a + b}`);
+console.log(`a - b = ${a - b}`);
+console.log(`a x b = ${a * b}`);
+console.log(`a / b = ${a / b}`);
+console.log(`O resto de a / b = ${a % b}`);
