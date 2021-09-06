44b8358a8120f983a071cac3846cd5da1195a8c3
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2021-09-06 12:44:17 -0300

    Exercício 9 OK.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio9.js
@@ -0,0 +1,11 @@
+// Programa 8: Define três números em constantes e retorna true se pelo menos uma das três for ímpar. Caso contrário, retorna false.
+
+const number1 = 7;
+const number2 = 5;
+const number3 = 2;
+
+if (((number1 % 2) !== 0) || ((number2 % 2) !== 0) || ((number3 % 2) !== 0)){
+    console.log('true');
+} else {
+    console.log('false');
+}
