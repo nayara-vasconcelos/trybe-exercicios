b30d491fac6010c846d6074ee7dbac75bc400743
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2021-09-05 23:25:22 -0300

    Atualização Exercício 1.

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -14,8 +14,10 @@
 
 
 // 1. Função para mudar o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos.
-function changeTextOfATag(tag, index, text){
-document.getElementsByTagName(tag)[index].innerText = text;
+function changeTextOfATag(tagString, indexNumber, textString){
+    document.getElementsByTagName(tagString)[indexNumber].innerText = textString;
 }
 
 changeTextOfATag('p', 1, 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat repudiandae sed amet quas corrupti libero voluptatem natus soluta modi quae suscipit rerum reprehenderit beatae accusamus, quaerat ratione, qui laudantium voluptate.');
+
+// Função para mudar a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109) = #4ca46d).
