58c011cc461633776b226cbc9ef9b07417d3c018
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2021-09-06 12:38:45 -0300

    Exercício 8 refatorado.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio8.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio8.js
@@ -4,8 +4,14 @@ const number1 = 7;
 const number2 = 5;
 const number3 = 0;
 
+/*
 if (((number1 % 2) === 0) || ((number2 % 2) === 0) || ((number3 % 2) === 0)) {
     console.log('true');
 } else {
     console.log('false');
 }
+*/
+
+// Programa 8 refatorado:
+const numberIsEven = (((number1 % 2) === 0) || ((number2 % 2) === 0) || ((number3 % 2) === 0));
+console.log(numberIsEven);
