fa2d389286c11daf13835d3efbcbe79221a0cbd4
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2021-10-10 02:10:12 -0300

    Exercício bônus 2.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus.js
@@ -28,3 +28,31 @@ for (let index1 = (numbers.length - 1); index1 > 0; index1 -= 1) {
 }
 
 console.log(numbers);
+
+
+// Exercício 2: Ordenação decrescente por bubble sort.
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//     if (numbers[index] > numbers[secondIndex]) {
+//       let position = numbers[index];
+//       numbers[index] = numbers[secondIndex];
+//       numbers[secondIndex] = position;
+//       console.log(numbers);
+//     }
+//   }
+// } 
+
+
+for (let index1 = (numbers.length - 1); index1 > 0; index1 -= 1) {
+  for (let index2 = 0; index2 < index1; index2 += 1) {
+    if (numbers[index2] < numbers[index2 +1]) {
+      let aux = numbers[index2];
+      numbers[index2] = numbers[index2 + 1];
+      numbers[index2 + 1] = aux;
+      console.log(numbers);
+    }
+  }
+}
+
+console.log(numbers);
