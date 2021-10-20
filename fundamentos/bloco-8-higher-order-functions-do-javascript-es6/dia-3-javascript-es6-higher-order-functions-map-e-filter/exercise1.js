03a087736137b6d4d6df14f330bb94935360f13f
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2021-10-20 10:05:18 -0300

    feat: exercicio 1 - criação de array usando .map()

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
@@ -60,3 +60,11 @@ const books = [
     releaseYear: 1928,
   },
 ];
+
+// 1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+// Dica: Use a função map
+const formattedBookNames = () => {
+  return books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
+}
+
+console.log(formattedBookNames());
