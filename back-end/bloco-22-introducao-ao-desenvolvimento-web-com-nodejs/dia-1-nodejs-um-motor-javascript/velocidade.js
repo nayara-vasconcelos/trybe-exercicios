9d4aea1457c2ce6d8fa5f52a05fa197f39f2bcfc
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-04-21 20:35:56 -0300

    feat:  adiciona script para calcular velocidade
    Adiciona funcoes para calcular velocidade media e mostrar resultado
    na tela.

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/velocidade.js
@@ -0,0 +1,15 @@
+const readline = require('readline-sync');
+
+const calculateAvgSpeedSI = (distance, time) => {
+  const avgSpeed = distance / time;
+  return avgSpeed;
+};
+
+const showAvgSpeed = () => {
+  const distance = readline.questionInt('Qual a distância percorrida em metros? ');
+  const time = (readline.questionInt('\nQual o intervalo de tempo em minutos? ') * 60);
+  const avgSpeed = calculateAvgSpeedSI(distance, time).toFixed(2);
+  console.log(`\nA velocidade média é ${avgSpeed} m/s.\n`);
+};
+
+showAvgSpeed();
