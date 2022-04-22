04e83632bb36c980c04a9fe57401f99dbea951ee
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-04-21 21:19:16 -0300

    feat: adiciona script para jogo de sorteio
    Adiciona funcoes para sortear um numero aleatorio entre 0 e 10
    e para jogar advinhacao.

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/sorteio.js
@@ -0,0 +1,28 @@
+const readline = require('readline-sync');
+
+const sortNumber = () => {
+  const sortedNumber = Math.round((Math.random() * 10));
+  return sortedNumber;
+};
+
+const playGuessTheNumber = () => {
+  let playAgain = false;
+  do {
+    const sortedNumber = sortNumber();
+    let bet = readline.questionInt('\nVocê consegue acertar o número sorteado?\nEscolha um número entre 0 e 10.\nAposta: ');
+
+    while(bet > 10 || bet < 0) {
+      bet = readline.questionInt('\nNúmero inválido! Escolha um número entre 0 e 10.\nAposta: ');
+    }
+
+    if(bet === sortedNumber) {
+      console.log('\nParabéns, número correto!');
+    } else {
+      console.log(`\nOpa, não foi dessa vez. O número era ${sortedNumber}.`);
+    }
+
+    playAgain = readline.keyInYN('\nVocê gostaria de tentar novamente? ');
+  } while (playAgain);
+};
+
+playGuessTheNumber();
